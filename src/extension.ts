6d6cffe2e37d7cab244ec9e6764739cadfc55b03
DinaSaller <dinas@daatsolutions.co.il>
2021-10-06 11:56:30 +0300

    update

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import * as vscode from 'vscode';
-// import  "form.html";
+import './middleware/sdkconnection';
 import { DepNodeProvider, Dependency } from './nodeDependencies';
 import { JsonOutlineProvider } from './jsonOutline';
 import { FtpExplorer } from './ftpExplorer';
@@ -28,91 +28,6 @@ function connect(gateway, clientNumber, sapUserId, password) {
 
 
 }
-
-function getWebviewContent() {
-	return `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-	  <meta charset="UTF-8">
-	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	  <title>Cat Coding</title>
-  </head>
-  <body>
-    <form >
-    <label for="fname"><b> enter the Protocol (as Http or Https)</b></label><br>
-    <input type="text" id="protocol" style="margin-top:1%; margin-bottom: 1%; width: 50%;"  name="fname" ><br>
-	<label for="fname"><b> enter the URL for the SAP Gateway System (as server.com:port or server.com:port )</b></label><br>
-    <input type="text" id="gateway" style="margin-top:1%; margin-bottom: 1%; width: 50%;"  name="fname" ><br>
-    <label for="lname" ><b>Please enter the client number</b></label><br>
-    <input type="text" id="clientNumber" name="clientNumber"  style="margin-top:1%; margin-bottom: 1%; width:20%;"><br>
-    <label for="lname" ><b>Please enter the SAP UserId </b></label><br>
-    <input type="text" id="sapUserId" name="sapUserId"  style="margin-top:1%; margin-bottom: 1%; width: 20%;"><br>
-    <label for="lname" ><b>Lastly Please enter the SAP  UserId Password</b></label><br>
-    <input type="password" id="password" name="password"  style="margin-top:1%; margin-bottom: 1%; width: 20%;"><br>
-    <input type="button" id="test" value="Test connection" onclick="testConnection();" style="margin-top: 2%;">
-  </form> 
-  	  <h1 id="lines-of-code-counter"></h1>
-  
-	  <script>
-	  function testConnection() {
-		const gateway = document.getElementById("gateway").value;
-	    const protocol = document.getElementById("protocol").value;
-		const clientNumber = document.getElementById("clientNumber").value;
-		const sapUserId = document.getElementById("sapUserId").value;
-		const password = document.getElementById("password").value;
-		const value = document.getElementById('lines-of-code-counter');
-		value.textContent = btoa(sapUserId+':'+password);
-		const url="https://localhost:3005/api/Meister/Login/"+protocol+"/"+gateway+"/"+clientNumber;
-		value.textContent=url;
-		fetch(url, {
-			method: 'POST',
-			headers: {
-			  'Content-Type': 'application/json',
-			  'Authorization': Basic+' '+btoa(sapUserId+':'+password)
-			}
-		  }).then(res => {
-			console.log('xcstr');
-		  });
-	  }
-		
-	  </script>
-  </body>
-  </html>`;
-}
-// function getWebviewContent() {
-// 	return `<!DOCTYPE html>
-//   <html lang="en">
-//   <head>
-// 	  <meta charset="UTF-8">
-// 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-// 	  <title>Cat Coding</title>
-//   </head>
-//   <body>
-//   <form action="/action_page.php">
-//   <label for="fname"><b> enter the URL for the SAP Gateway System (as Http://server.com:port or Https://server.com:port )</b></label><br>
-//   <input type="text" id="getway" style="margin-top:1%; margin-bottom: 1%; width: 50%;"  name="fname" ><br>
-//   <label for="lname" ><b>Please enter the client number</b></label><br>
-//   <input type="text" id="lname" name="lname"  style="margin-top:1%; margin-bottom: 1%; width:20%;"><br>
-//   <label for="lname" ><b>Please enter the SAP UserId </b></label><br>
-//   <input type="text" id="lname" name="lname"  style="margin-top:1%; margin-bottom: 1%; width: 20%;"><br>
-//   <label for="lname" ><b>Lastly Please enter the SAP  UserId Password</b></label><br>
-//   <input type="password" id="lname" name="lname"  style="margin-top:1%; margin-bottom: 1%; width: 20%;"><br>
-//   <input type="submit" id="test" value="Test connection" onclick="testConnection();" style="margin-top: 2%;">
-// </form> 
-// <script>
-// console.log("lllll);
-
-//   function testConnection() {
-// console.log("kkkk);
-
-//   } 
-
-// </script>
-//   </body>
-
-//   </html>`;
-// }
-
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log(vscode.workspace.workspaceFolders);
@@ -132,8 +47,82 @@ export function activate(context: vscode.ExtensionContext) {
 				enableScripts: true
 			}
 		);
-		panel.webview.html = getWebviewContent();
+		panel.webview.html = getWebviewContent(panel.webview, context);
 	});
+
+	function getWebviewContent(webview: vscode.Webview, context: vscode.ExtensionContext) {
+
+		// const jsFilePath = webview.asWebviewUri(vscode.Uri.joinPath(extensionContext.extensionUri, 'hello', 'connectSdk.ts'));
+
+		const scriptPathOnDisk = vscode.Uri.joinPath(context.extensionUri, 'middleware', 'sdkconnection.ts');
+
+		const scriptUri = (scriptPathOnDisk).with({ 'scheme': 'vscode-resource' });
+		console.log(scriptUri);
+
+		// Local path to css styles
+		// const styleResetPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css');
+		// const stylesPathMainPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css');
+
+		// Uri to load styles into webview
+		// const stylesResetUri = webview.asWebviewUri(styleResetPath);
+		// const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+		return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta
+  http-equiv="Content-Security-Policy"
+  content="default-src 'none'; img-src ${webview.cspSource} https:; script-src ${webview.cspSource}; style-src ${webview.cspSource};"
+/>
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Cat Coding</title>
+  </head>
+  <body>
+    <form >
+    <label for="fname"><b> enter the Protocol (as Http or Https)</b></label><br>
+    <input type="text" id="protocol" style="margin-top:1%; margin-bottom: 1%; width: 50%;"  name="fname" ><br>
+	<label for="fname"><b> enter the URL for the SAP Gateway System (as server.com:port or server.com:port )</b></label><br>
+    <input type="text" id="gateway" style="margin-top:1%; margin-bottom: 1%; width: 50%;"  name="fname" ><br>
+    <label for="lname" ><b>Please enter the client number</b></label><br>
+    <input type="text" id="clientNumber" name="clientNumber"  style="margin-top:1%; margin-bottom: 1%; width:20%;"><br>
+    <label for="lname" ><b>Please enter the SAP UserId </b></label><br>
+    <input type="text" id="sapUserId" name="sapUserId"  style="margin-top:1%; margin-bottom: 1%; width: 20%;"><br>
+    <label for="lname" ><b>Lastly Please enter the SAP  UserId Password</b></label><br>
+    <input type="password" id="password" name="password"  style="margin-top:1%; margin-bottom: 1%; width: 20%;"><br>
+    <input type="button" id="test" value="Test connection" onclick="testConnection();" style="margin-top: 2%;">
+  </form> 
+  	  <h1 id="lines-of-code-counter"></h1>
+  
+		<script src="${scriptUri}"></script>
+
+  </body>
+  </html>`;
+	}
+
+	// <script>
+	// 	  function testConnection() {
+	// 		const gateway = document.getElementById("gateway").value;
+	// 	    const protocol = document.getElementById("protocol").value;
+	// 		const clientNumber = document.getElementById("clientNumber").value;
+	// 		const sapUserId = document.getElementById("sapUserId").value;
+	// 		const password = document.getElementById("password").value;
+	// 		const value = document.getElementById('lines-of-code-counter');
+	// 		value.textContent = btoa(sapUserId+':'+password);
+	// 		const url="https://localhost:3005/api/Meister/Login/"+protocol+"/"+gateway+"/"+clientNumber;
+	// 		value.textContent=url;
+	// 		fetch(url, {
+	// 			method: 'POST',
+	// 			headers: {
+	// 			  'Content-Type': 'application/json',
+	// 			  'Authorization': Basic+' '+btoa(sapUserId+':'+password)
+	// 			}
+	// 		  }).then(res => {
+	// 			console.log('success');
+	// 		  });
+	// 	  }
+
+	// 	  </script>
+
 
 
 
---
+++ src/middleware/sdkconnection.ts
@@ -0,0 +1,44 @@
+
+// (function () {
+// 	// const counter = document.getElementById('lines-of-code-counter');
+
+// 	console.log("hhhhhhhhhhhhhhhhhhhhh");
+// 	// const counter = /** @type {HTMLElement} */ (document.getElementById('test'));
+// 	// // const saveAsPngButton = document.getElementById('test');
+// 	// counter.addEventListener('click', () => {
+// 	// 	console.log("clicked");
+
+// 	// });
+
+// }());
+
+// 		const gateway = document.getElementById("gateway").value;
+// 	    const protocol = document.getElementById("protocol").value;
+// 		const clientNumber = document.getElementById("clientNumber").value;
+// 		const sapUserId = document.getElementById("sapUserId").value;
+// 		const password = document.getElementById("password").value;
+// 		const value = document.getElementById('lines-of-code-counter');
+// 		value.textContent = btoa(sapUserId+':'+password);
+// 		const url="https://localhost:3005/api/Meister/Login/"+protocol+"/"+gateway+"/"+clientNumber;
+// 		value.textContent=url;
+// 		fetch(url, {
+// 			method: 'POST',
+// 			headers: {
+// 			  'Content-Type': 'application/json',
+// 			  'Authorization': Basic+' '+btoa(sapUserId+':'+password)
+// 			}
+// 		  }).then(res => {
+// 			console.log('success');
+// 		  });
+// 	  }
+console.log("hello");
+// .addEventListener('click', ()=>{
+// 	console.log("click");
+	
+// } );
+
+// const saveAsPngButton = document.getElementById('test');
+// saveAsPngButton.addEventListener('click', ()=>{
+// 	console.log("clicked");
+
+// });
